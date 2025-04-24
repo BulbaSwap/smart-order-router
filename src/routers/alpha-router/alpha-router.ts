f1a2b87c90574a76e186c5439124c50ffc62f9e7
Clayon <admin@github.xyz>
2025-04-24 12:26:34 +0800

    Release version 2.0.5

--- src/routers/alpha-router/alpha-router.ts
+++ src/routers/alpha-router/alpha-router.ts
@@ -553,6 +553,8 @@ export class AlphaRouter
           break;
         case ChainId.CELO:
         case ChainId.CELO_ALFAJORES:
+        case ChainId.HOLESKY:
+        case ChainId.MORPH:
           this.onChainQuoteProvider = new OnChainQuoteProvider(
             chainId,
             provider,
@@ -1525,7 +1527,7 @@ export class AlphaRouter
     const v2ProtocolSpecified = protocols.includes(Protocol.V2);
     const v2SupportedInChain = V2_SUPPORTED.includes(this.chainId);
     const shouldQueryMixedProtocol = protocols.includes(Protocol.MIXED) || (noProtocolsSpecified && v2SupportedInChain);
-    const mixedProtocolAllowed = [ChainId.MAINNET, ChainId.GOERLI, ChainId.HOLESKY].includes(this.chainId) &&
+    const mixedProtocolAllowed = [ChainId.MAINNET, ChainId.GOERLI, ChainId.HOLESKY, ChainId.MORPH].includes(this.chainId) &&
       tradeType === TradeType.EXACT_INPUT;
 
     const beforeGetCandidates = Date.now();
